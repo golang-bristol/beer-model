53bfe8b8491e15d33f4f6f9b5089c976a12087b4
Kat <kat.zien@brightpearl.com>
2016-11-08 19:27:38 +0000

    Fixed tests after the move to the beer-model repo

--- model_test.go
+++ model_test.go
@@ -3,13 +3,15 @@ package model_test
 import (
 	"testing"
 	"time"
+
+	model "github.com/golang-bristol/beer-model"
 )
 
 func TestReview(t *testing.T) {
 	review := model.Review{1, 100, "Tim", "Lawrence", 7, "This is a great beer", time.Now()}
 
-	if review.Beer_id != 55 {
-		t.Error("Expected 55 got", review.Beer_id)
+	if review.Beer_id != 100 {
+		t.Error("Expected 100 got", review.Beer_id)
 	}
 
 }
